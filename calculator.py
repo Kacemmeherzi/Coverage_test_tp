b8cdd913471a5eb2a44904d504cb5ff870a0eb70
kacem <kacemmeherzi52@gmail.com>
2024-09-16 15:00:57 +0100

    calculator class added

---
+++ calculator.py
@@ -0,0 +1,28 @@
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("error ")
+        return a / b
+
+
+
+    def calculate(self, a, b, operator):
+        if operator == '+':
+            return self.add(a, b)
+        elif operator == '-':
+            return self.subtract(a, b)
+        elif operator == '*':
+            return self.multiply(a, b)
+        elif operator == '/':
+            return self.divide(a, b)
+        else:
+            raise ValueError("errrrrroooouuuuuuuuuuuur")
